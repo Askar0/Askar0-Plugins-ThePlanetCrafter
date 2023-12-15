71420118aaee0e1ed64c6f23ddd23376eac1f930
Askar0 <46961993+Askar0@users.noreply.github.com>
2023-12-15 19:51:59 +0800

    Create Askar0-Plugins-ThePlanetCrafter Repository

---
+++ Askar0-Plugins/Plugin.cs
@@ -0,0 +1,33 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using Mono.Cecil.Cil;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+
+namespace Askar0_Plugins;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    static ConfigEntry<bool> isDebug;
+    static ConfigEntry<Key> isKey;
+
+
+    static ManualLogSource logger;
+
+    public const string HashKey = "{}";
+
+    /// <summary>
+    /// 
+    /// </summary>
+    private void Awake()
+    {
+        // Plugin startup logic
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+        isDebug = Config.Bind("General", "Debug", true, "Debugging Enabled?");
+        isKey = Config.Bind("General", "KeyBind", Key.K, "Dump Debug Info Key");
+        logger = base.Logger;
+
+    }
+}
